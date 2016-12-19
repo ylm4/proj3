2f6bbb5f052839419a4091ce89cba242c509027c
Yanira Morales <ylm4@njit.edu>
2016-12-19 17:34:51 -0500

    index

--- pro3/index.php
+++ pro3/index.php
@@ -11,7 +11,7 @@ include("username.user.php");
 <link rel= "stylesheet" href="css/style.css" />
 </head>
 <body>
-<p><a href="home.user.php">Home</a>&nbsp|&nbsp<a
+<p><a href="home.user.php">Dashboard</a>&nbsp|&nbsp<a
 href="index.php">Profile</a>&nbsp|&nbsp<a href="users.user.php">Users</a>&nbsp|&nbsp<a
 href="logout.user.php">Logout</a>
 <h1><?php echo $_SESSION['username']; ?>'s Profile</h1>
