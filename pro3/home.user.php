2aede061a9731b7261dfdb1cdf4a8adf71a72393
Yanira Morales <ylm4@njit.edu>
2016-12-19 17:34:07 -0500

    home

--- pro3/home.user.php
+++ pro3/home.user.php
@@ -7,11 +7,11 @@ include("username.user.php");
 <!DOCTYPE html>
 <html>
 <head>
-<title>Dashboard - Secured Page</title>
+<title>Dashboard (Secured)</title>
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-<p><a href="home.user.php">Home</a>&nbsp|&nbsp<a href="users.user.php">Users</a>&nbsp|&nbsp<a href="index.php">Profile</a>&nbsp|&nbsp<a
+<p><a href="home.user.php">Dashboard</a>&nbsp|&nbsp<a href="users.user.php">Users</a>&nbsp|&nbsp<a href="index.php">Profile</a>&nbsp|&nbsp<a
 href="logout.user.php">Logout</a>
 <div class="formh">
 <p></p>
